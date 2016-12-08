9cc548c5c44feceb54b072e1209af69365d49c8d
Mathieu Basille <mathieu@mablap2.ad.ufl.edu>
2016-12-08 15:09:38 -0500

    Added spdep.

--- install.selected.R
+++ install.selected.R
@@ -1,9 +1,8 @@
-install.packages("devtools")
-library("devtools")
-install_github(c("basille/basr", "basille/hab", "basille/seasonality"))
-
-install.packages(c("adehabitatHS", "Cairo", "CircStats", "colorRamps", "coxme", "dismo", "foreign", "formatR", "fortunes", "knitr", "leaflet", "lme4", "lsmeans", "maptools", "move", "MuMIn", "popbio", "randomForest", "raster", "rasterVis", "reshape2", "RMark", "rmarkdown", "roxygen2", "rpostgisLT", "RPostgreSQL", "rworldmap", "rworldxtra", "scales", "SDMTools", "shiny", "spacetime", "spatstat", "tidyverse"))
+install.packages(c("adehabitatHS", "Cairo", "CircStats", "colorRamps", "cowplot", "coxme", "devtools", "dismo", "foreign", "formatR", "fortunes", "gridExtra", "Hmisc", "knitr", "leaflet", "lme4", "lsmeans", "maptools", "move", "MuMIn", "popbio", "randomForest", "raster", "rasterVis", "reshape2", "RMark", "rmarkdown", "roxygen2", "rpostgisLT", "RPostgreSQL", "rworldmap", "rworldxtra", "scales", "spdep", "SDMTools", "shiny", "spacetime", "spatstat", "tidyverse"))
 
 if (gdal)
     install.packages(c("rgdal", "rgeos"))
+
+library("devtools")
+install_github(c("basille/basr", "basille/hab", "basille/seasonality"))
 
