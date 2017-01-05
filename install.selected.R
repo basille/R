d32bb4537a7a7a4ede06f64066f2dd22a6826641
Mathieu Basille <basille@ufl.edu>
2017-01-05 15:19:42 -0500

    Update list of packages (not. using Spatial Task View).

--- install.selected.R
+++ install.selected.R
@@ -1,7 +1,23 @@
-install.packages(c("adehabitatHS", "Cairo", "CircStats", "colorRamps", "cowplot", "cowsay", "coxme", "devtools", "dismo", "foreign", "formatR", "gridExtra", "Hmisc", "knitr", "leaflet", "lme4", "lsmeans", "maptools", "move", "MuMIn", "popbio", "randomForest", "raster", "rasterVis", "reshape2", "RMark", "rmarkdown", "roxygen2", "rpostgisLT", "RPostgreSQL", "rworldmap", "rworldxtra", "scales", "spdep", "SDMTools", "shiny", "spacetime", "spatstat", "tidyverse"))
+## Install CTV (CRAN Task Views)
+install.packages("ctv")
+library("ctv")
 
-if (gdal)
-    install.packages(c("rgdal", "rgeos"))
+## Install Spatial Task View (core)
+## https://cran.r-project.org/web/views/Spatial.html
+install.views("Spatial", coreOnly = TRUE)
+## From time to time, needs the following to install new packages
+## added to the view:
+## update.views("Spatial", coreOnly = TRUE)
 
+## adehabitat, rpostgis and friends
+install.packages("adehabitatHS", "rpostgisLT")
+
+## Tidyverse and cowplot
+install.packages(c("cowplot", "tidyverse"))
+
+## Other packages
+install.packages(c("broom", "Cairo", "CircStats", "colorRamps", "cowplot", "cowsay", "coxme", "devtools", "dismo", "foreign", "formatR", "ggfortify", "gridExtra", "Hmisc", "knitr", "leaflet", "lme4", "lsmeans", "move", "MuMIn", "popbio", "randomForest", "rasterVis", "reshape2", "RMark", "rmarkdown", "roxygen2", "rworldmap", "rworldxtra", "scales", "SDMTools", "shiny", "tidyverse"))
+
+## MabLab packages on GitHub
 library("devtools")
 install_github(c("basille/basr", "basille/hab", "basille/seasonality"))
